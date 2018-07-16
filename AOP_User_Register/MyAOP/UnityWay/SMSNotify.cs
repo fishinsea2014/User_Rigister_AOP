1817fe0861d4976393360a2bd3cc95dc91e16b48
Jason Qu <fishinsea2014@gmail.com>
2018-07-16 23:38:43 +1200

    Add a texting behavior that happens after all the actions

---
+++ AOP_User_Register/MyAOP/UnityWay/SMSNotify.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Interception.InterceptionBehaviors;
+using Unity.Interception.PolicyInjection.Pipeline;
+
+namespace MyAOP.UnityWay
+{
+    public class SMSNotify : IInterceptionBehavior
+    {
+        public IEnumerable<Type> GetRequiredInterfaces()
+        {
+            return Type.EmptyTypes;
+        }
+
+        public IMethodReturn Invoke(IMethodInvocation input, GetNextInterceptionBehaviorDelegate getNext)
+        {
+            IMethodReturn methodReturn = getNext()(input, getNext);
+            Console.WriteLine("Text user for notification");
+            return methodReturn;
+        }
+
+
+
+        public bool WillExecute
+        {
+            get { return true; }
+        }
+
+    }
+}
